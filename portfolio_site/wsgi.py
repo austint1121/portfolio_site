89ca1b63bedc5b9420d09d6efe61b1a140d9ad87
austint1121 <austin.turner1121@gmail.com>
2021-06-14 13:24:06 -0400

    Adding conf files

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
-
+sys.path.append('/opt/bitnami/projects/portfolio_site')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/portfolio_site/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_site.settings")
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
