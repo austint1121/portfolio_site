13e7aa9ff4f90897f49c541292fc1ff2228c2265
austint1121 <austin.turner1121@gmail.com>
2021-06-14 13:02:05 -0400

    Adding Server IP as allowed host

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['23.22.239.184']
 
 
 # Application definition
