f76d9a7885495800fb1a9905644f9a81c24346b0
austint1121 <austin.turner1121@gmail.com>
2021-06-15 10:15:50 -0400

    Removing lines in wsgi

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -8,11 +8,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-sys.path.append('/opt/bitnami/projects/portfolio_site')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/portfolio_site/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_site.settings")
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJECT.settings')
 application = get_wsgi_application()
