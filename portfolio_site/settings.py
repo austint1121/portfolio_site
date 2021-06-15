35f30f6b12f1fbc304aba340e3ddefb322637a2c
austint1121 <austin.turner1121@gmail.com>
2021-06-15 11:45:25 -0400

    Changed debug to false and removed dev ip as allowed host

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['23.22.239.184','mturner.me', '127.0.0.1']
+ALLOWED_HOSTS = ['23.22.239.184','mturner.me']
 
 
 # Application definition
