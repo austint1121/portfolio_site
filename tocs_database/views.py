21979990132c563e39cab36dbe3e8774e8cc04fd
austint1121 <austin.turner1121@gmail.com>
2021-06-23 11:29:26 -0400

    Updated tocs page with retractable tables for each location

--- tocs_database/views.py
+++ tocs_database/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Enemy
+from .models import Enemy, Location
 # Create your views here.
 
 def landing(request):
     enemy = Enemy.objects.all()
-    context = {'enemy': enemy}
+    location = Location.objects.all()
+    context = {'enemy': enemy, 'location': location}
     return render(request, 'users.html', context)
