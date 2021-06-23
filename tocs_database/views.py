beafd551b3de9e602a0afb8f0c0fce8dafe41251
austint1121 <austin.turner1121@gmail.com>
2021-06-23 10:24:25 -0400

    Model and html testing

--- tocs_database/views.py
+++ tocs_database/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Enemy
 # Create your views here.
+
 def landing(request):
-    return render(request, 'index.html')
+    enemy = Enemy.objects.all()
+    context = {'enemy': enemy}
+    return render(request, 'users.html', context)
