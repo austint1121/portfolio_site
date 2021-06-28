ae274b77e05d07e06113e3c0ca057171b812a0f1
austint1121 <austin.turner1121@gmail.com>
2021-06-28 10:27:49 -0400

    First stable release for Portfolio Site. Added 21 TODO tasks. Switched Debug off.

--- tocs_database/models.py
+++ tocs_database/models.py
@@ -84,68 +84,13 @@ class Enemy(models.Model):
     def __str__(self):
         return self.name
 
-# class ElementalEfficacy(models.Model):
-#     """Model for elemental efficacy for a paticular monster"""
-#     earth = models.IntegerField(default=100)
-#     water = models.IntegerField(default=100)
-#     fire = models.IntegerField(default=100)
-#     wind = models.IntegerField(default=100)
-#     #  Time, space, and mirage weakness are not present for all enemies.
-#     Time = models.IntegerField(null=True, blank=True)
-#     space = models.IntegerField(null=True, blank=True)
-#     mirage = models.IntegerField(null=True, blank=True)
-
-# class SepithDropped(models.Model):
-#     """Model for sepith drop for a monster"""
-#     earth = models.IntegerField(null=True, blank=True)
-#     water = models.IntegerField(null=True, blank=True)
-#     fire = models.IntegerField(null=True, blank=True)
-#     wind = models.IntegerField(null=True, blank=True)
-#     Time = models.IntegerField(null=True, blank=True)
-#     space = models.IntegerField(null=True, blank=True)
-#     mirage = models.IntegerField(null=True, blank=True)
-#     mass = models.IntegerField(null=True, blank=True)
-
-
+# TODO: Decide if item drops should be kept as a model.
 class ItemDrop(models.Model):
     """Model for Item drops"""
     name = models.CharField(max_length=50, help_text='Name of item')
     drop_chance = models.FloatField(help_text='Enter drop chance as decimal')
     def __str__(self):
         return self.name
-
-# class UnbalanceEfficacy(models.Model):
-#     """Model for a monster's unbalance chance"""
-#     efficacy = (
-#         ('1/5', 1),
-#         ('2/5', 2),
-#         ('3/5', 3),
-#         ('4/5', 4),
-#         ('5/5', 5),
-#     )
-#
-#     slash = models.IntegerField(choices=efficacy, default=1)
-#     thrust = models.IntegerField(choices=efficacy, default=1)
-#     pierce = models.IntegerField(choices=efficacy, default=1)
-#     strike = models.IntegerField(choices=efficacy, default=1)
-
-# class StatusEfficacy(models.Model):
-#     """Status ailement for a monster"""
-#     poision = models.IntegerField(default=100)
-#     seal = models.IntegerField(default=100)
-#     mute = models.IntegerField(default=100)
-#     blind = models.IntegerField(default=100)
-#     sleep = models.IntegerField(default=100)
-#     burn = models.IntegerField(default=100)
-#     freeze = models.IntegerField(default=100)
-#     petrify = models.IntegerField(default=100)
-#     faint = models.IntegerField(default=100)
-#     confuse = models.IntegerField(default=100)
-#     deathblow = models.IntegerField(default=100)
-#     nightmare = models.IntegerField(default=100)
-#     at_delay = models.IntegerField(default=100)
-#     vanish = models.IntegerField(default=100)
-#     s_down = models.IntegerField(default=100)
 
 class Skill(models.Model):
     """Arts / skills for enemies"""
--- tocs_database/views.py
+++ tocs_database/views.py
@@ -9,6 +9,7 @@ def landing(request):
     return render(request, 'users.html', context)
 
 def enemy_profile(request, enemy_id):
+    # TODO: Change url to enemy names rather then an id
     enemy = get_object_or_404(Enemy, id=enemy_id)
     context = {'enemy': enemy, 'title':f'{enemy.name} Combat profile'}
     return render(request,'profile.html', context)
