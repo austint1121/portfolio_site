ae6a2e263312c3bc76dc632212d435b2ecd8c467
austint1121 <austin.turner1121@gmail.com>
2021-07-01 13:21:10 -0400

    Added projects.html, Portfolio section is complete.

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('success/', views.email_success, name='success'),
     path('about_me/', views.about_me, name='about me'),
     path('contact/', views.contact, name='contact'),
+    path('projects/', views.projects, name='projects'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from main.forms import ContactForm
 from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
+    # TODO: create method for form post
     if request.method =='POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -71,3 +72,22 @@ def contact(request):
         'form':form
     }
     return render(request, 'contact.html', context)
+
+def projects(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['name']
+            user_email = form.cleaned_data['email']
+            user_message = form.cleaned_data['message']
+            subject = f"Message from '{user_name}' via mturner.me Contact Form"
+            message = user_message + f'\n\nReply email is {user_email}'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = ['austin.turner1121@gmail.com']
+            send_mail(subject, message, email_from, recipient_list)
+            return redirect('success/')
+    else: form = ContactForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'projects.html', context)
