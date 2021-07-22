8d8847bbd8f5533354f05ce007336483ed4e9671
austint1121 <austin.turner1121@gmail.com>
2021-07-22 13:43:08 -0400

    Debug off for production version.

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -11,7 +11,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['23.22.239.184','mturner.me', '127.0.0.1']
 
